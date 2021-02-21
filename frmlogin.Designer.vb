26749f298db34314b800dbc24493caf19f7bef92
Apichai Butdee <hamhapichai@gmail.com>
2021-02-21 19:17:32 +0700

    Initial commit

---
+++ frmlogin.Designer.vb
@@ -0,0 +1,124 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmlogin
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmlogin))
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtusername = New System.Windows.Forms.TextBox()
+        Me.txtpassword = New System.Windows.Forms.TextBox()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.btnlogin = New System.Windows.Forms.Button()
+        Me.btncancel = New System.Windows.Forms.Button()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(191, 44)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(60, 15)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Username"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(191, 84)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(57, 15)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Password"
+        '
+        'txtusername
+        '
+        Me.txtusername.Location = New System.Drawing.Point(281, 41)
+        Me.txtusername.Name = "txtusername"
+        Me.txtusername.Size = New System.Drawing.Size(100, 23)
+        Me.txtusername.TabIndex = 2
+        '
+        'txtpassword
+        '
+        Me.txtpassword.Location = New System.Drawing.Point(281, 81)
+        Me.txtpassword.Name = "txtpassword"
+        Me.txtpassword.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
+        Me.txtpassword.Size = New System.Drawing.Size(100, 23)
+        Me.txtpassword.TabIndex = 3
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(12, 12)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(150, 140)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 4
+        Me.PictureBox1.TabStop = False
+        '
+        'btnlogin
+        '
+        Me.btnlogin.Location = New System.Drawing.Point(191, 127)
+        Me.btnlogin.Name = "btnlogin"
+        Me.btnlogin.Size = New System.Drawing.Size(75, 23)
+        Me.btnlogin.TabIndex = 5
+        Me.btnlogin.Text = "Login"
+        Me.btnlogin.UseVisualStyleBackColor = True
+        '
+        'btncancel
+        '
+        Me.btncancel.Location = New System.Drawing.Point(306, 127)
+        Me.btncancel.Name = "btncancel"
+        Me.btncancel.Size = New System.Drawing.Size(75, 23)
+        Me.btncancel.TabIndex = 6
+        Me.btncancel.Text = "Cancel"
+        Me.btncancel.UseVisualStyleBackColor = True
+        '
+        'frmlogin
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(399, 168)
+        Me.Controls.Add(Me.btncancel)
+        Me.Controls.Add(Me.btnlogin)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.txtpassword)
+        Me.Controls.Add(Me.txtusername)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "frmlogin"
+        Me.Text = "frmlogin"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtusername As TextBox
+    Friend WithEvents txtpassword As TextBox
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents btnlogin As Button
+    Friend WithEvents btncancel As Button
+End Class
---
+++ frmlogin.vb
@@ -0,0 +1,31 @@
+﻿Imports System.Data
+Imports System.Data.OleDb
+Public Class frmlogin
+    Dim conn As New OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=E:\visualbasic\cafePOS\posdatabase.accdb;Persist Security Info=True")
+    Dim cmd As OleDbCommand
+    Private Sub btnlogin_Click(sender As Object, e As EventArgs) Handles btnlogin.Click
+        Me.Hide()
+        Dim sql = "SELECT username ,password FROM StaffTBL WHERE username='" & txtusername.Text & "' AND password='" & txtpassword.Text & "'"
+        cmd = New OleDbCommand(sql, conn)
+        conn.Open()
+        Dim dr As OleDbDataReader = cmd.ExecuteReader
+        Try
+            If dr.Read = False Then
+                MessageBox.Show("Login failed...")
+                Me.Show()
+            Else
+                MessageBox.Show("Login Successfully...")
+                Dim frmDialogue As New frmmain
+                frmDialogue.ShowDialog()
+                Me.Close()
+            End If
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+        conn.Close()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles btncancel.Click
+        Me.Close()
+    End Sub
+End Class
